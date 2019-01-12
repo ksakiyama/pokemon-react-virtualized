d16b48788f4a992644b2d4a53180eb1c7c5f4fc6
ksakiyama <k.sakiyama1@gmail.com>
2019-01-13 08:22:24 +0900

    reducerのtypeを追加

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,10 @@ import {
   SET_ALL_POKEMON,
   FILTER,
   SET_FILTER_NAME,
-  SET_FILTER_TYPE
+  SET_FILTER_TYPE,
+  GET_ALL_POKEMON_REQUEST,
+  GET_ALL_POKEMON_SUCCESS,
+  GET_ALL_POKEMON_FAILURE
 } from "../constants";
 
 const initialState = {
@@ -41,6 +44,14 @@ export default function(state = initialState, action) {
         filterType: action.payload.filterType.slice()
       };
     }
+    case GET_ALL_POKEMON_FAILURE: {
+      return {
+        ...state,
+        error: action.payload.error
+      };
+    }
+    case GET_ALL_POKEMON_REQUEST:
+    case GET_ALL_POKEMON_SUCCESS:
     default:
       return state;
   }
