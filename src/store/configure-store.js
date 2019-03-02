fd211bbe458358289661ceab9220fbe3c6a283ec
ksakiyama <k.sakiyama1@gmail.com>
2019-03-02 16:25:17 +0900

    Revert redux-thunk

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from "../reducers";
 
 export default function configureStore(initialState = {}) {
-  const middlewares = [];
+  const middlewares = [thunkMiddleware];
 
   let composeEnhancers = compose;
 
